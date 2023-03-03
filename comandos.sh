1f32b0a1e0986a36d16b099d4db3a73a78b4accb
igorferrati <igotferrati93@gmail.com>
2023-03-03 18:39:28 -0300

    Desafio 01

--- comandos.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-spawn-fcgi -s /var/run/fcgiwrap.socket /usr/bin/fcgiwrap
-chmod 777 /run/fcgiwrap.socket
-chown www-data.www-data /usr/lib/cgi-bin/teste2.cgi
-chmod +x /usr/lib/cgi-bin/teste2.cgi
-nginx -g "daemon off;"
